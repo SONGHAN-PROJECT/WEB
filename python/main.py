ccc7cf01b84626c944dc0855f78c8c4569a27754
sysong0516 <sysong777@naver.com>
2024-05-12 19:57:56 +0900

    구글로그인 기능 추가 error : 로그인을 2번째 할때부터 리디렉션 url 불일치뜸

--- python/main.py
+++ python/main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from datetime import timedelta
+
+from flask import Flask, render_template, url_for, session, redirect, request
+from flask_oauthlib.client import OAuth
 
 app = Flask(__name__, template_folder='../web', static_folder='../web/static')
 
@@ -7,18 +10,59 @@ PORT = 8080
 DEBUG = True
 
 print('실행')
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=1)
+app.secret_key = 'your_secret_key_here'
+oauth = OAuth(app)
+google = oauth.remote_app(
+    'google',
+    consumer_key='947304049541-4atb6apclsjndp4euaq4ku8sk98hj2bh.apps.googleusercontent.com',
+    consumer_secret='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',
+    request_token_params={
+        'scope': 'email'
+    },
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    logged_in = check_token()
+    # index.html 템플릿 렌더링 시 로그인 상태 정보를 전달
+    return render_template('index.html', logged_in=logged_in)
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return google.authorize(callback=url_for('authorized', _external=True))
+
+@app.route('/check_token')
+def check_token():
+    return 'google_token' in session
+
+@app.route('/login/authorized')
+def authorized():
+    resp = google.authorized_response()
+    if resp is None or resp.get('access_token') is None:
+        return '로그인 실패'
+
+    # 로그인이 성공하면 리디렉션을 수행
+    session['google_token'] = request.args.get('google_token')
+    return redirect(url_for('home'))
+
+
+ # 리디렉션을 처리하는 엔드포인트
+# @app.route('/redirected')
+# def redirected():
+#     return redirect(url_for('/'))
+
 
 @app.route("/board")
 def board():
-    return render_template("board.html")
+    logged_in = check_token()
+    # index.html 템플릿 렌더링 시 로그인 상태 정보를 전달
+    return render_template('board.html', logged_in=logged_in)
 @app.route("/signup")
 def signup():
     return render_template("signup.html")

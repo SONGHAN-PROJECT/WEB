7502576443486ecc35a9c6f06a94e3b09489d405
sysong0516 <sysong777@naver.com>
2024-05-21 18:07:58 +0900

    로그인로그아웃처리 에러:구글 리다이렉션 미스매치

--- python/main.py
+++ python/main.py
@@ -1,9 +1,10 @@
 from datetime import timedelta
-
+from flask_sslify import SSLify
 from flask import Flask, render_template, url_for, session, redirect, request
 from flask_oauthlib.client import OAuth
 
 app = Flask(__name__, template_folder='../web', static_folder='../web/static')
+sslify = SSLify(app)
 
 HOST = 'localhost'
 PORT = 8080
@@ -18,24 +19,32 @@ google = oauth.remote_app(
     consumer_key='947304049541-4atb6apclsjndp4euaq4ku8sk98hj2bh.apps.googleusercontent.com',
     consumer_secret='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',
     request_token_params={
-        'scope': 'email'
+        'scope': 'email',
     },
     base_url='https://www.googleapis.com/oauth2/v1/',
     request_token_url=None,
     access_token_method='POST',
     access_token_url='https://accounts.google.com/o/oauth2/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_url='https://localhost:8080/login/authorized',
 )
 
 @app.route("/")
 def home():
     logged_in = check_token()
+    print(logged_in)
     # index.html 템플릿 렌더링 시 로그인 상태 정보를 전달
     return render_template('index.html', logged_in=logged_in)
 
 @app.route("/login")
 def login():
     return google.authorize(callback=url_for('authorized', _external=True))
+
+@app.route('/logout')
+def logout():
+    # 세션을 비움
+    session.pop('google_token', None)
+    # 로그아웃 후 어떤 페이지로 리디렉션할지 설정
+    return redirect(url_for('home'))
 
 @app.route('/check_token')
 def check_token():
@@ -45,10 +54,12 @@ def check_token():
 def authorized():
     resp = google.authorized_response()
     if resp is None or resp.get('access_token') is None:
+        print('실패',resp)
         return '로그인 실패'
 
     # 로그인이 성공하면 리디렉션을 수행
-    session['google_token'] = request.args.get('google_token')
+    print('성공')
+    session['google_token'] = (resp['access_token'], '')
     return redirect(url_for('home'))
 
 
@@ -68,4 +79,6 @@ def signup():
     return render_template("signup.html")
 
 if __name__=='__main__':
-    app.run(host=HOST,port=PORT,threaded=DEBUG)
+    ssl_cert = 'C:/Users/82102/PycharmProjects/WEB/server.crt'
+    ssl_key = 'C:/Users/82102/PycharmProjects/WEB/server.key'
+    app.run(host=HOST,port=PORT,threaded=DEBUG,ssl_context=(ssl_cert, ssl_key))
